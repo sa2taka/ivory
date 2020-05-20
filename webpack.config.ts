72f2c4592e6f1345ea98fa0b2f3c4a7ca0eedc65
sa2taka <sa2taka@gmail.com>
2020-05-20 20:57:24 +0900

    :wrench: set webpack.config.ts to configure eslint

--- webpack.config.ts
+++ webpack.config.ts
@@ -10,6 +10,15 @@ module.exports = {
   module: {
     rules: [
       {
+        // コンパイルの事前に eslint による
+        // 拡張子 .ts または .tsx の場合
+        test: /\.tsx?$/,
+        // 事前処理であることを示す
+        enforce: 'pre',
+        // TypeScript をコードチェックする
+        loader: 'eslint-loader',
+      },
+      {
         test: /\.tsx?$/,
         use: 'ts-loader',
       },
