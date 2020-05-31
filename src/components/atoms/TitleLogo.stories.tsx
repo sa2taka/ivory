691dddb07637839bfd3b9b4f6c2d3a1cadf567f9
sa2taka <sa2taka@gmail.com>
2020-06-01 01:16:37 +0900

    :recycle: Divide title logo

---
+++ src/components/atoms/TitleLogo.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { TitleLogo as Component } from './TitleLogo';
+export default {
+  title: 'atom',
+};
+export const TitleLogo = () => <Component />;
--- src/components/organisms/layoutHeader.tsx
+++ src/components/organisms/layoutHeader.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './LayoutHeader.scss';
-import 'public/ivory-logo.png';
+import { TitleLogo } from '@/components/atoms/TitleLogo';
 
 export function LayoutHeader() {
   return (
     <header className="LayoutHeader h-16">
       <div className="h-full .z-0 flex relative items-center">
-        <div className="flex ml-4 items-center">
-          <img src="ivory-logo.png" className="h-8 w-8"></img>
-          <span className="ml-2 text-2xl font-semibold">Ivory</span>
-        </div>
+        <TitleLogo />
       </div>
     </header>
   );
