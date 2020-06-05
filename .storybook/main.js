b2ec0f721ffd133d3b1214466258101eb4124425
sa2taka <sa2taka@gmail.com>
2020-06-06 02:16:19 +0900

    :wrench: Add storybook knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,11 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs/register',
+  ],
   webpackFinal: async (config) => {
     console.log(config.resolve);
     config.module.rules.push({
