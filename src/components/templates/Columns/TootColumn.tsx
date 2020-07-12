d552960ecae60e61a7921aa050f9a7ae99d92a9f
sa2taka <sa2taka@gmail.com>
2020-07-13 00:53:27 +0900

    :sparkles: add TootColumn(does not work

---
+++ src/components/templates/Columns/TootColumn.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { OneColumn } from '@/components/organisms/OneColumn';
+
+interface Props {}
+
+export const TootColumn: React.FC<Props> = (props) => {
+  return (
+    <OneColumn>
+      <TootArea />
+    </OneColumn>
+  );
+};
